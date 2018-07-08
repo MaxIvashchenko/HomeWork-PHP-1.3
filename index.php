e1633b422ad0f8f6e61090492303a986759fa171
MaxIvashchenko <40541533+MaxIvashchenko@users.noreply.github.com>
2018-07-08 22:55:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<html lang="ru">
+<meta charset="UTF-8">
+  
+<?php
+
+$continents = [
+    "Africa" => ["Hexaprotodon Falconer", "Syncerus caffer", "Ceratotherium Gray", "Lion"],
+    "South America" => ["Cryptonanus ignitus", "Microsciurus flaviventer", "Mazama", "Nasua Storr"],
+    "Ausrtalia" => ["Kangaroo", "Numbat", "Phascolarctos cinereus", "Platypus"],
+];
+
+echo '<h1>Название зверей</h1>';
+echo '<pre>';
+print_r($continents);
+echo '</pre>';
+
+// Пункт два
+$firstWord = [];
+$secondWord = [];
+$collocation = [];
+
+
+foreach($continents as $continent => $animal_types){
+     foreach($animal_types as $type){
+        $count = substr_count($type, ' ');
+            if(count === 1){
+            $collocation[$continent][]=$animal;
+              $word = explode($type, ' ');
+             $firstWord[] = $word [0];
+             $secondWord[] = word [1];
+             }
+        }
+    }
+
+ 
+echo '<h1>Животные с двумя словами</h1>';
+echo '<pre>';
+print_r($collocation);
+echo '</pre>';
+
+// Пункт три
+
